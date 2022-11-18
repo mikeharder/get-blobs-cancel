b77817430e89e509100f8311d15ced32d93c2fcc
Mike Harder <mharder@microsoft.com>
2022-11-18 12:16:33 -0800

    Add app

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Azure.Storage.Blobs;
+
+namespace GetBlobsCancel
+{
+    internal class Program
+    {
+        // Config
+        private const string ContainerName = "samples";
+        private const int MinDelayMilliseconds = 1;
+        private const int MaxDelayMilliseconds = 20;
+
+        static void Main(string[] args)
+        {
+            var connectionString = Environment.GetEnvironmentVariable("STORAGE_CONNECTION_STRING") ??
+                throw new InvalidOperationException("STORAGE_CONNECTION_STRING not set");
+
+            var containerClient = new BlobContainerClient(connectionString, ContainerName);
+
+            var random = new Random();
+            while (true)
+            {
+                var delay = random.Next(MinDelayMilliseconds, MaxDelayMilliseconds);
+                using (var cts = new CancellationTokenSource(millisecondsDelay: delay))
+                {
+                    try
+                    {
+                        // Enumerate collection to ensure all BlobItems are downloaded
+                        foreach (var _ in containerClient.GetBlobs(cancellationToken: cts.Token))
+                        {
+                        }
+                        Console.WriteLine($"[{DateTime.Now:hh:mm:ss.fff}] completed");
+                    }
+                    catch (TaskCanceledException)
+                    {
+                        Console.WriteLine($"[{DateTime.Now:hh:mm:ss.fff}] cancelled");
+                    }
+                }
+            }
+        }
+    }
+}
